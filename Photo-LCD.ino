36f952a0d445dc1b8a3dfa87192a944633d7f381
Alba-Elektronica <64310350+albaelektronica@users.noreply.github.com>
2021-03-26 15:06:28 +0100

    Update and rename Microph_LCD_v1.ino to Photo-LCD.ino

--- Photo-LCD.ino
+++ Photo-LCD.ino
@@ -2,7 +2,7 @@
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 
-int sensorPin = A0; // select the input pin for the potentiometer
+int sensorPin = A0; // select the input pin 
 int ledPin = 13; // select the pin for the LED
 int sensorValue = 100; // variable to store the value coming from the sensor
 
